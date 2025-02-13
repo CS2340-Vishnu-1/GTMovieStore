bd262a6db5f7022ebc583f56d8b31ee932b458e8
Gabe Sankar <gabesankar1@gmail.com>
2025-02-13 18:03:26 -0500

    finished cart page and functions

--- cart/urls.py
+++ cart/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<int:id>/add/', views.add, name='cart.add'),
+    path('', views.index, name='cart.index'),
+    path('clear/', views.clear, name='cart.clear'),
 ]
